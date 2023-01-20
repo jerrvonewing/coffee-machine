d6b30785c95289ce23221ba490ac542ac873dd75
jerrvon <jerrvonewing.cs@gmail.com>
2023-01-20 14:26:10 -0600

    init commit

---
+++ index.py
@@ -0,0 +1,27 @@
+# Prompt user by asking “​What would you like? (espresso/latte/cappuccino):”​
+# Check user input to decide what to do next
+# The prompt should show every time action has completed
+
+
+# Turn off the Coffee Machine by entering “​off”​to the prompt.
+
+
+# Print report
+
+
+# Check resources
+# When the user chooses a drink, the program should check if there are enough resources to make that drink
+
+
+# Process coins
+
+
+# Complete transaction
+# Check that the user has inserted enough money to purchase the drink they selected.
+# Update report
+# Check if change needs to be dispersed
+
+
+# Make coffee 
+# Update resources
+# Thank customer
